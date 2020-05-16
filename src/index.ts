418f60ee475ee6eea63980fe55f81b72915075c7
ergouwang <mrergouwang@gmail.com>
2020-05-16 22:17:48 +0800

    EventHub 1.0 完成

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,35 @@
 class EventHub  {
-    on(){}
-    emit(){}
+
+    cache = {}
+    /*
+    * '楚天都市报' [fn1,fn2,fn3]
+    * '羊城晚报' [fn1,fn2,fn3]
+    * cache 里面存放的就是事件名和事件对象
+    *  */
+
+    on(eventName,fn){
+        //把 fn 推进 this.cache[eventName] 数组
+        //如果没有事件存在，我们初始化一下
+        if(this.cache[eventName] === undefined){
+            this.cache[eventName] = []
+        }
+
+        const array = this.cache[eventName]
+        array.push(fn)
+    }
+
+    emit(eventName){
+        // 把 this.cache[eventName] 数组里面的 fn 全部依次调用
+        let array = this.cache[eventName]
+        if(array === undefined){
+            array = []
+        }
+
+        array.forEach(fn=>{
+            fn()
+        })
+
+    }
 }
 
 export default EventHub
--- test/index.ts
+++ test/index.ts
@@ -3,11 +3,11 @@ import EventHub  from "../src/index";
 const eventHub = new EventHub()
 
 //第一个断言
-console.assert(eventHub instanceof Object()  === true,
+console.assert(eventHub instanceof Object  === true,
     'eventHub 是一个对象')
 
 eventHub.on('xxx',()=>{
-
+    console.log('我被调用了');
 })
 
 eventHub.emit('xxx')
