68e9823558e502dfd56ca0c0a5bd2fc1808fe02f
ergouwang <mrergouwang@gmail.com>
2020-05-16 22:25:34 +0800

    EventHub 1.1 完成1.0版本优化

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,24 @@
-class EventHub  {
+class EventHub {
 
     cache = {}
+
     /*
     * '楚天都市报' [fn1,fn2,fn3]
     * '羊城晚报' [fn1,fn2,fn3]
+    * cache = {'楚天都市报':[fn1,fn2,fn3]}
     * cache 里面存放的就是事件名和事件对象
     *  */
 
-    on(eventName,fn){
+    on(eventName, fn) {
         //把 fn 推进 this.cache[eventName] 数组
         //如果没有事件存在，我们初始化一下
-        if(this.cache[eventName] === undefined){
-            this.cache[eventName] = []
-        }
-
-        const array = this.cache[eventName]
-        array.push(fn)
+        this.cache[eventName] = this.cache[eventName] || []
+        this.cache[eventName].push(fn)
     }
 
-    emit(eventName){
+    emit(eventName) {
         // 把 this.cache[eventName] 数组里面的 fn 全部依次调用
-        let array = this.cache[eventName]
-        if(array === undefined){
-            array = []
-        }
-
-        array.forEach(fn=>{
-            fn()
-        })
-
+        (this.cache[eventName] || []).forEach(fn => fn())
     }
 }
 
