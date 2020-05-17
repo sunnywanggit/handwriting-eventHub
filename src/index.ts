4da45771748b7e174ed89d32f3ea9c051a356889
ergouwang <mrergouwang@gmail.com>
2020-05-17 12:08:33 +0800

    EventHub 用 ts 重写完成

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 class EventHub {
 
-    cache = {}
+    private  cache : {[key:string]:Array<(data:unknown)=>void>} = {}
+
 
     /*
     * '楚天都市报' [fn1,fn2,fn3]
@@ -9,16 +10,34 @@ class EventHub {
     * cache 里面存放的就是事件名和事件对象
     *  */
 
-    on(eventName, fn) {
+    //订阅
+    on(eventName:string, fn:(data:unknown)=>void) {
         //把 fn 推进 this.cache[eventName] 数组
         //如果没有事件存在，我们初始化一下
-        this.cache[eventName] = this.cache[eventName] || []
-        this.cache[eventName].push(fn)
+        (this.cache[eventName] || []).push(fn)
     }
 
-    emit(eventName) {
+    // ? 在这里的意思是说 data 可以为空
+    //发布
+    emit(eventName:string, data?:unknown) {
         // 把 this.cache[eventName] 数组里面的 fn 全部依次调用
-        (this.cache[eventName] || []).forEach(fn => fn())
+        (this.cache[eventName] || []).forEach(fn => fn(data))
+    }
+
+    //取消订阅
+    off(eventName:string, fn:(data:unknown)=>void) {
+        this.cache[eventName] = this.cache[eventName] || []
+        //用 index 来记住你要取消订阅的事件
+        let index = undefined
+        for (let i = 0; i < this.cache[eventName].length; i++) {
+            if (this.cache[eventName][i] === fn) {
+                index = i
+                break
+            }
+        }
+
+        if (index === undefined) return;
+        this.cache[eventName].splice(index, 1)
     }
 }
 
--- test/index.ts
+++ test/index.ts
@@ -6,11 +6,31 @@ const eventHub = new EventHub()
 console.assert(eventHub instanceof Object  === true,
     'eventHub 是一个对象')
 
-eventHub.on('xxx',()=>{
+eventHub.on('xxx',(data)=>{
     console.log('我被调用了');
+    console.log(data);
 })
 
-eventHub.emit('xxx')
+eventHub.emit('xxx','我是emit的第二个参数')
 
 
+
+const eventHub2 = new EventHub()
+
+let fn2 = ()=>{
+    console.log('我是fn2， 我应该是不执行的');
+}
+eventHub2.on('yyy',fn2)
+
+eventHub2.off('yyy',fn2)
+
+eventHub2.emit('yyy')
+
+/*
+* 你这个只接受一个参数，我要传两个参数怎么办？直接传一个数组就好了
+*
+* 你故意留一个你能解决的坑给他问
+*
+* 为什么safeCache 不行？
+* */
 
