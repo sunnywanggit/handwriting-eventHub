ec46d7ca049bff10d94e284fe344a5ce2f592eb4
ergouwang <330035385@qq.com>
2020-08-10 11:39:16 +0800

    优化代码，添加注释

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,50 @@
 class EventHub {
 
-    private  cache : {[key:string]:Array<(data:unknown)=>void>} = {}
-
+    private cache: { [key: string]: Array<(data: unknown) => void> } = {};
 
     /*
-    * '楚天都市报' [fn1,fn2,fn3]
-    * '羊城晚报' [fn1,fn2,fn3]
+    * eventName '楚天都市报'  fn [fn1,fn2,fn3]
+    * eventName '羊城晚报' fn [fn1,fn2,fn3]
     * cache = {'楚天都市报':[fn1,fn2,fn3]}
-    * cache 里面存放的就是事件名和事件对象
-    *  */
+    * cache 里面存放的就是事件名和该事件名所对应的事件回调
+    */
 
-    //订阅
-    on(eventName:string, fn:(data:unknown)=>void) {
-        //把 fn 推进 this.cache[eventName] 数组
-        //如果没有事件存在，我们初始化一下
-        (this.cache[eventName] || []).push(fn)
+    /*
+    eventName 你需要订阅的事件名
+    fn 事件发生的时候，你要做的事情
+     */
+    on(eventName: string, fn: (data: unknown) => void) {
+        /*
+        把 fn 推进 this.cache[eventName] 数组
+        如果没有事件存在，我们初始化一下
+         */
+        (this.cache[eventName] || []).push(fn);
     }
 
-    // ? 在这里的意思是说 data 可以为空
     //发布
-    emit(eventName:string, data?:unknown) {
+    emit(eventName: string, data?: unknown) {
         // 把 this.cache[eventName] 数组里面的 fn 全部依次调用
-        (this.cache[eventName] || []).forEach(fn => fn(data))
+        if (this.cache[eventName] === undefined) return;
+        this.cache[eventName].forEach(fn => fn(data));
     }
 
     //取消订阅
-    off(eventName:string, fn:(data:unknown)=>void) {
-        this.cache[eventName] = this.cache[eventName] || []
+    off(eventName: string, fn: (data: unknown) => void) {
+        // 把 fn 从 this.cache[eventName] 数组里面中删除
+        this.cache[eventName] = this.cache[eventName] || [];
         //用 index 来记住你要取消订阅的事件
-        let index = undefined
+        let index = undefined;
         for (let i = 0; i < this.cache[eventName].length; i++) {
             if (this.cache[eventName][i] === fn) {
-                index = i
-                break
+                index = i;
+                break;
             }
         }
 
         if (index === undefined) return;
-        this.cache[eventName].splice(index, 1)
+        this.cache[eventName].splice(index, 1);
     }
 }
 
-export default EventHub
+export default EventHub;
 
--- test/index.ts
+++ test/index.ts
@@ -1,30 +1,40 @@
-import EventHub  from "../src/index";
+import EventHub from '../src/index';
 
-const eventHub = new EventHub()
+test1('eventHub 是一个对象');
+test2('eventHub 有 on 和 emit 属性');
+test3('eventHub 可以取消事件订阅');
 
-//第一个断言
-console.assert(eventHub instanceof Object  === true,
-    'eventHub 是一个对象')
-
-eventHub.on('xxx',(data)=>{
-    console.log('我被调用了');
-    console.log(data);
-})
-
-eventHub.emit('xxx','我是emit的第二个参数')
-
-
-
-const eventHub2 = new EventHub()
-
-let fn2 = ()=>{
-    console.log('我是fn2， 我应该是不执行的');
+function test1(message) {
+    const eventHub = new EventHub();
+    console.log(message);
+    console.assert(eventHub instanceof Object,);
 }
-eventHub2.on('yyy',fn2)
 
-eventHub2.off('yyy',fn2)
+function test2(message) {
+    const eventHub = new EventHub();
+    console.log(message);
+    //必须要先订阅再发布，如果你先发布再订阅就有可能没病订阅上
+    eventHub.on('xxx', (data) => {
+        //TODO 为什么这句话不会执行？
+        console.log('xxx');
+        console.assert(data === '我是emit的第二个参数');
+    });
+    eventHub.emit('xxx', '我是emit的第二个参数');
+}
 
-eventHub2.emit('yyy')
+
+function test3(message) {
+    console.log(message);
+    const eventHub = new EventHub();
+
+    let fn2 = () => {
+        console.log('我是fn2， 我应该是不执行的,因为我的订阅在中途被取消了');
+    };
+    eventHub.on('yyy', fn2);
+    eventHub.off('yyy', fn2);
+    eventHub.emit('yyy');
+}
+
 
 /*
 * 你这个只接受一个参数，我要传两个参数怎么办？直接传一个数组就好了
