d02a63ae84bf58e948a8f0656771d3ac8847d7c0
ergouwang <330035385@qq.com>
2020-08-10 13:20:14 +0800

    完善测试用例

--- test/index.ts
+++ test/index.ts
@@ -14,25 +14,40 @@ function test2(message) {
     const eventHub = new EventHub();
     console.log(message);
     //必须要先订阅再发布，如果你先发布再订阅就有可能没病订阅上
+    let called = false;
     eventHub.on('xxx', (data) => {
-        //TODO 为什么这句话不会执行？
-        console.log('xxx');
+        called = true;
         console.assert(data === '我是emit的第二个参数');
     });
     eventHub.emit('xxx', '我是emit的第二个参数');
+
+    setTimeout(()=>{
+        console.log('我执行了');
+        console.assert(called === true)
+    },1000)
 }
+
+
 
 
 function test3(message) {
     console.log(message);
     const eventHub = new EventHub();
 
+
+    let called = false;
     let fn2 = () => {
+        called = true;
         console.log('我是fn2， 我应该是不执行的,因为我的订阅在中途被取消了');
     };
     eventHub.on('yyy', fn2);
     eventHub.off('yyy', fn2);
     eventHub.emit('yyy');
+
+    setTimeout(()=>{
+        console.log('我没有执行');
+        console.assert(called === false)
+    },1000)
 }
 
 
